951f72ed52030f2e2be49392a0e9773172ea51ea
Jason <jasontzs007@gmail.com>
2022-09-07 22:33:09 +0800

    ccccc

--- newgame/settings.py
+++ newgame/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'newgame.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'football',                      
+        'USER': 'postgres',                     
+        'PASSWORD': 'Tzs12345$$',                  
+        'HOST': 'localhost',                      
+        'PORT': '5432',                      
     }
 }
 

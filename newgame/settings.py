3abde662ecf14550316d6ef440ff644bd4b78318
Jason <jasontzs007@gmail.com>
2022-09-08 11:10:39 +0800

    firstcommit

--- newgame/settings.py
+++ newgame/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '****************&&&&&&**********&&&************************************'
+SECRET_KEY = 'ads'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
         'NAME': 'football',                      
-        'USER': '***************',                     
-        'PASSWORD': '********************************',                  
-        'HOST': '*****************',                      
-        'PORT': '*****************',                      
+        'USER': 'postgres',                     
+        'PASSWORD': '',                  
+        'HOST': '',                      
+        'PORT': '',                      
     }
 }
 
